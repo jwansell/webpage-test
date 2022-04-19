47fb372d530482d92efa88dbd91bc60c6c5957ff
Joseph Ansell <joseph.ansell@yourmeds.net>
2022-04-19 10:40:59 +0100

    challenge 11 completed

--- php/PdoConnect.php
+++ php/PdoConnect.php
@@ -1,14 +1,16 @@
 <?php
 
+require('../vendor/autoload.php'); // In order to make use of the composer packages we need to require the autoload.php file
+
+// Load the .env file using the Package we downloaded
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../'); 
+$dotenv->load();
+
 function createPdoConnection() {
-	$servername = "localhost";
-	$username = "root";
-	$password = "password";
-	try {
-		$conn = new PDO("mysql:host=$servername;dbname=webpage", $username, $password);
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		return $conn;
-	} catch(PDOException $e) {
-		echo "Connection failed: " . $e->getMessage();
-	}
+	$servername = $_ENV['DB_HOST'];
+	$username = $_ENV['DB_USER'];
+	$password = $_ENV['DB_PASS'];
+	$conn = new PDO("mysql:host=$servername;dbname=webpage", $username, $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $conn;
 }
